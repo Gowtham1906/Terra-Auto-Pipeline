bcc440109515197fc960f22d6bcd7b6b0417c0c5
S.GOWTHAM <88620210+Gowtham1906@users.noreply.github.com>
2024-10-18 22:16:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "region" {
+  description = "Region"
+}
+
 variable "vpc_configs" {
   description = "Map of VPC configurations to create"
   type = map(object({
@@ -8,22 +12,7 @@ variable "vpc_configs" {
   }))
 }
 
-variable "region" {
-  description = "Region"
-}
-
 variable "existing_vpc_ids" {
   description = "List of existing VPC IDs to import"
   type        = list(string)
-}
-
-variable "imported_vpc_configs" {
-  description = "Imported VPC configurations"
-  type = map(object({
-    cidr_block           = string
-    enable_dns_support   = bool
-    enable_dns_hostnames = bool
-    tags                 = map(string)
-  }))
-  default = {}
 }
