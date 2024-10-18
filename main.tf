1299d18167810a3a6e72c59da64db7c96ec25374
S.GOWTHAM <88620210+Gowtham1906@users.noreply.github.com>
2024-10-18 22:13:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,19 +11,11 @@ resource "aws_vpc" "my_vpc" {
   tags                 = each.value.tags
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket = "my-infra-bucket-161024"
-#     key    = "terraform/statefile.tfstate"
-#     region = "us-east-1" 
-#   }
-# }
+ terraform {
+   backend "s3" {
+     bucket = "my-infra-bucket-161024"
+     key    = "terraform/statefile.tfstate"
+     region = "us-east-1" 
+   }
+ }
 
-
-resource "aws_vpc" "my_existing_vpc" {
-  for_each = var.imported_vpc_configs
-  cidr_block = each.value.cidr_block
-  enable_dns_support = each.value.enable_dns_support
-  enable_dns_hostnames = each.value.enable_dns_hostnames
-  tags = each.value.tags
-}
